0ae7abade6817be364e3930b52ac2f25d08288e6
Paolo Freuli <paolo.freuli@gmail.com>
2016-06-29 22:46:27 +0200

    updateIssue - not yet working

--- lib/redelix.ex
+++ lib/redelix.ex
@@ -11,14 +11,8 @@ defmodule Redelix do
     decoded_response=decode_get_response(HTTPotion.get "#{url}/projects/#{project_name}/wiki/index.json", [basic_auth: auth()])
     return_decoded_response(decoded_response, "wiki_pages")
  end
- 
- #CREDIT: thanks to Dogbert (http://stackoverflow.com/users/320615/dogbert)
- #http://stackoverflow.com/questions/38037325/elixir-how-to-post-on-a-rest-api-redmine-with-httpotion
- def createIssue(issue) do
-   body = Plug.Conn.Query.encode(issue)
-   HTTPotion.post("#{url}/issues.json", [body: body, basic_auth: auth()])
- end
- 
+  
+  
   
  def getIssue(issue_id) do
     decoded_response=decode_get_response(HTTPotion.get "#{url}/issues/#{issue_id}.json", [basic_auth: auth()])
@@ -34,6 +28,28 @@ def getIssues(query_string) do
     decoded_response=decode_get_response(HTTPotion.get "#{url}/issues.json", [basic_auth: auth(), query: query_string]  ) 
     return_decoded_response(decoded_response, "issues")
 end
+
+
+ #CREDIT: thanks to Dogbert (http://stackoverflow.com/users/320615/dogbert)
+ #http://stackoverflow.com/questions/38037325/elixir-how-to-post-on-a-rest-api-redmine-with-httpotion
+ #~ def createIssue(issue) do
+   #~ HTTPotion.post("#{url}/issues.json", [body: Plug.Conn.Query.encode(issue), basic_auth: auth()])
+ #~ end
+def createIssue(issue) do
+ value = %{issue: issue}
+ body = Plug.Conn.Query.encode(value) 
+ HTTPotion.post("#{url}/issues.json", [body: body, basic_auth: auth()])
+ end
+ 
+
+ def updateIssue(issue_id, issue_changes) do
+   #value = %{issue: issue_changes}
+  # value = %{issue: issue_changes}
+   changes = Plug.Conn.Query.encode(issue_changes) 
+   HTTPotion.post("#{url}/issues/#{issue_id}.json", [body: changes, basic_auth: auth()])
+ end
+
+
 
 
  def url() do
